d07817ac167c673d2c278c4024c97679bc4a47bb
HoudaFet <houda.fetian@gmail.com>
2023-06-15 11:14:22 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - program that prints exactly a line using puts function 
- * return: 0(success)
+ * main - A program that prints exactly a line using puts function 
+ * Return: 0(Success)
  */
 int main(void)
 {
