90b7ae0351976ec01051cc4eb42feb4406a92aee
HoudaFet <houda.fetian@gmail.com>
2023-06-15 11:34:57 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints exactly a line using puts function 
- * Return: 0(Success)
- */
+* main - A program that prints exactly a line using puts function.
+* Return: 0(Success).
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
