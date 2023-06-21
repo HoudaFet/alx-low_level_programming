bf72817654798071a526f053001cebf0aa7b05c7
HoudaFet <houda.fetian@gmail.com>
2023-06-21 05:47:01 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet.
+ * Return: Always 0 (Success).
+ */
+int main(void);
+{
+	char c;
+	char D;
+
+	c = 'a'
+	D = 'A'
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (D <= 'Z')
+	{
+		putchar(D);
+		D++;
+	}
+return (0);
