94bf521e7284892c62c7dfdc79ca2af8ec009cf3
HoudaFet <houda.fetian@gmail.com>
2023-06-21 07:57:02 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase.
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	while
+	(c <= 'z', c != e, c != q) {
+		putchar(c);
+		c++;
+	}
+return (0);
+}
