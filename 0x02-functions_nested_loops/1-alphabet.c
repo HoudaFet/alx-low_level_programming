8cfba48cd6fc1d6431bf39f12c47d96130715e75
HoudaFet <houda.fetian@gmail.com>
2023-06-22 07:54:26 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - A function that prints the alphabet in lowercase
+ * print_alphabet - A function that prints the alphabet in lowercase
  * Return: Always 0
  */
 void print_alphabet(void)
